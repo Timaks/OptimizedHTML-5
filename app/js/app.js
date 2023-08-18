f1dfb9d55c9dd009deab370d5fc29c814ba25579
Timaks <aksinya2016@gmail.com>
2023-08-18 17:54:50 +0300

    подготовка

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,6 @@
+import { gsap } from 'gsap'
+let cx, cy, mouseX, mouseY, posX, posY, clientX, clientY, dx, dy, tiltx, tilty, request, radius, degree
+
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
